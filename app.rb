25e43cc0e500f6fba6c0017f890c8bf3e1881786
Adam Agnaou <adam@fapfap.se>
2014-03-03 19:59:30 +0100

    Refactor base64 helper. Allows project to be run without a base64 folder.

--- app.rb
+++ app.rb
@@ -18,13 +18,16 @@ get '/email' do
   document.transform
 end
 
-def base64_images(whitelist = %w( .png .jpg .gif ), dir = "base64")
+def base64_images(whitelist = %w{ .png .jpg .gif })
   files = {}
+  directory = "#{ settings.root }/base64"
 
-  Dir.foreach("#{ settings.root }/#{ dir }") do |file|
-    if whitelist.include? File.extname(file)
-      key = File.basename(file, File.extname(file)).to_sym
-      files[key] = Base64.encode64(File.read([dir, file].join("/")))
+  if File.exists?("base64")
+    Dir.foreach(directory) do |file|
+      if whitelist.include? File.extname(file)
+        key = File.basename(file, File.extname(file)).to_sym
+        files[key] = Base64.encode64(File.read([directory, file].join("/")))
+      end
     end
   end
 
